53c602f523a918ff395117193f4df919c6c9c919
Anselm Baur <anselm.baur@belle2.org>
2022-12-30 14:25:20 +0100

    fixed build

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-#from b2analysis.tools import *
-#from b2analysis.efficiency import *
-#from b2analysis.elog import *
-#from b2analysis.ntuples import *
-#from b2analysis.admin import *
-#from b2analysis.pxd import *
-#from b2analysis.histogram import *
---
+++ src/b2analysis/__init__.py
@@ -0,0 +1,7 @@
+from b2analysis.tools import *
+from b2analysis.efficiency import *
+from b2analysis.elog import *
+from b2analysis.ntuples import *
+from b2analysis.admin import *
+from b2analysis.pxd import *
+from b2analysis.histogram import *
